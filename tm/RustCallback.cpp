6663f4243a90e64c94f304e3a7af4e312de81ac6
Ashoat Tevosyan <ashoat@gmail.com>
2023-05-12 11:34:36 +0200

    Introduce Cargo project for Rust code
    To build this, `cd rust && cargo build` (will be handled by build script in the next diff)

---
+++ tm/RustCallback.cpp
@@ -0,0 +1,19 @@
+#include "RustCallback.h"
+#include <future>
+#include <iostream>
+#include "RustPromiseManager.h"
+
+void stringCallback(rust::String error, uint32_t promiseID, rust::String ret) {
+  auto it = RustPromiseManager::instance.promises.find(promiseID);
+  if (it == RustPromiseManager::instance.promises.end()) {
+    return;
+  }
+
+  if (error.size()) {
+    RustPromiseManager::instance.rejectPromise(promiseID, std::string(error));
+  } else {
+    folly::dynamic retDyn;
+    retDyn = std::string(ret);
+    RustPromiseManager::instance.resolvePromise(promiseID, retDyn);
+  }
+}
---
+++ tm/RustCallback.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "cxx.h"
+
+void stringCallback(rust::String error, uint32_t promiseID, rust::String ret);
